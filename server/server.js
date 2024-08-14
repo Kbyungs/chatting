edd658bc0a0de0e1df6397d5d3663a8028de390f
Kbyungs <kbsoo0620@icloud.com>
2024-08-14 22:15:45 +0900

    main page & modal

--- server/server.js
+++ server/server.js
@@ -1,1 +1,26 @@
-console.log(123);
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front/chatting.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("user A connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+const PORT = 3000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
