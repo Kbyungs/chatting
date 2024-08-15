95ac0e9270f6d4d1645b1ca4ce59559e5eea9d47
Kbyungs <kbsoo0620@icloud.com>
2024-08-15 22:39:15 +0900

    create room

--- server/server.js
+++ server/server.js
@@ -4,8 +4,42 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const path = require("path");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 여기까지가 설정
+
+let room_id = [];
+
+function generateRoomId(room_id) {
+  function getRandNum() {
+    const min = 100000;
+    const max = 999999;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  let roomNumber;
+  do {
+    roomNumber = getRandNum();
+  } while (room_id.includes(roomNumber));
+  // console.log(roomNumber);
+  return roomNumber;
+}
+
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../front/index.html"));
+});
+
+app.get("/chat", (req, res) => {
   res.sendFile(path.join(__dirname, "../front/chatting.html"));
+});
+
+app.post("/create", (req, res) => {
+  var roomNum = generateRoomId(room_id);
+  // console.log(JSON.stringify(req.body));
+  console.log(req.body);
+  console.log(`${roomNum} 번 방 생성`);
+  room_id.push(roomNum);
+  res.json({ roomNum });
 });
 
 io.on("connection", (socket) => {
